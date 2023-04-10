7e8d0cb29668c0bb132f8d686a023a2af223eab6
dmikomarov <77617042+dmikomarov@users.noreply.github.com>
2023-04-10 20:53:34 +0300

    Проблема с запуском драйвера

---
+++ src/test/java/helpers/BrowserStackHelper.java
@@ -0,0 +1,26 @@
+package helpers;
+import org.aeonbits.owner.ConfigFactory;
+import config.MobileConfig;
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+import static helpers.CustomAllureListener.withCustomTemplates;
+public class BrowserStackHelper {
+
+  public static String getVideoUrl (String sessionId) {
+
+    MobileConfig config = ConfigFactory.create(MobileConfig.class);
+
+    String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+    return given()
+            .log().all()
+            .filter(withCustomTemplates())
+            .auth().basic(config.getUser(), config.getKey())
+            .when()
+            .get(url)
+            .then()
+            .log().all()
+            .statusCode(200)
+            .extract().path("automation_session.video_url");
+  }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,42 @@
+package tests;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import drivers.BrowserstackMobileDriver;
+import helpers.Attach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+  @BeforeAll
+  static void beforeAll() {
+    Configuration.browser = BrowserstackMobileDriver.class.getName();
+    Configuration.browserSize = null;
+    open(); // костыль для конфигурации селенида для мобильной автоматизации
+//        Configuration.timeout = 15000;
+//        Configuration.pageLoadTimeout = 15000;
+  }
+
+  @BeforeEach
+  void addListener() {
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    open(); // костыль для инициализации селенида для мобильной автоматизации
+  }
+
+  @AfterEach
+  void afterEach() {
+
+    String sessionId = Selenide.sessionId().toString();
+
+    Attach.pageSource();
+
+    closeWebDriver();
+
+    Attach.addVideo(sessionId);
+  }
+}
