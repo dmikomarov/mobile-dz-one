b865afbde557636202ba225997e90597a7cf5ee2
dmikomarov <77617042+dmikomarov@users.noreply.github.com>
2023-04-11 12:05:27 +0300

    заменил device на env

--- src/test/java/config/MobileConfig.java
+++ src/test/java/config/MobileConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 
-@Config.Sources({"classpath:${device}.properties"})
+@Config.Sources({"classpath:${env}.properties"})
 
 public interface  MobileConfig extends Config {
   @Key("user")
