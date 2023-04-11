ece39101f6f26b0ae71704f5d612fbe692807bce
alx.krw <sashkir7@mail.ru>
2023-04-11 15:55:51 +0600

    Add sys prop env validate

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,5 @@
 package tests;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -12,30 +13,31 @@ import helpers.Attach;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
-  @BeforeAll
-  static void beforeAll() {
-    Configuration.browser = BrowserstackMobileDriver.class.getName();
-    Configuration.browserSize = null;
-    // костыль для конфигурации селенида для мобильной автоматизации
-//        Configuration.timeout = 15000;
-//        Configuration.pageLoadTimeout = 15000;
-  }
 
-  @BeforeEach
-  void addListener() {
-    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    open(); // костыль для инициализации селенида для мобильной автоматизации
-  }
+    static {
+        if (System.getProperty("env") == null) {
+            throw new RuntimeException("Забыли передать System Properties [env]");
+        }
+    }
 
-  @AfterEach
-  void afterEach() {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
 
-    String sessionId = Selenide.sessionId().toString();
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
 
-    Attach.pageSource();
+    @AfterEach
+    void afterEach() {
+        String sessionId = Selenide.sessionId().toString();
+        Attach.pageSource();
+        closeWebDriver();
+        Attach.addVideo(sessionId);
+    }
 
-    closeWebDriver();
-
-    Attach.addVideo(sessionId);
-  }
 }
